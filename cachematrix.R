f3f3e1f81845a9c9b9e3b7fb334728235ce65f5a
smatismi <47929013+smatismi@users.noreply.github.com>
2019-05-19 10:57:01 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This is my code for assigment 2
+## It creates and caches a matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ mat1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+        nmat1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
 }
 
 
 ## Write a short comment describing this function
+##This is the code for the second part of assigment 2 
+##this function returns the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        I2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
 }
