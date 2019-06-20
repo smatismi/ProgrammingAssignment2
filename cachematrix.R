8f337b93624fc90928bcab19439d4a52f1091fe8
smatismi <47929013+smatismi@users.noreply.github.com>
2019-06-20 17:36:43 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-## This is my code for assigment 2
-## It creates and caches a matrix
 
-## Write a short comment describing this function
+## This is my code for assigment 2
+## It creates and caches a matrix and returns the inverse
+
+
 ##create matrix and cache the value
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -11,10 +10,16 @@ mat1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
         nmat1 <- matrix(c(6,2,8,4), nrow = 2, ncol = 2)
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                mat1 <<- NULL
         
 }
- 
+ get <- function() x
+        setinv <- function(inv) minv <<- inv
+        getinv <- function() minv     
+        list(set = set, get = get,
+             setinv= setinv,
+             getinv = getinv)
+}
  
 ## Write a short comment describing this function
 ##This is the code for the second part of assigment 2 
@@ -22,5 +27,14 @@ mat1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
 
 cacheSolve <- function(x, ...) {
        ## Return a matrix that is the inverse of 'x'
-        I2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+              m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setinv(m)
+        m
 }
+
